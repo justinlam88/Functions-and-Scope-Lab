04b8e40d131e61e2ab43f5bdd0b20b2bbf228c11
Justin <justinlam88@gmail.com>
2016-06-23 22:36:38 -0400

    lab 2

---
+++ lab2/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        parameterCount("animals");
+        stringLength("gold");
+        Scanner question = new Scanner(System.in);
+
+        System.out.println(command);
+        String answer = question.next();
+        userInput(answer);
+
+    }
+
+        public static void parameterCount(String... word){
+        System.out.println(word.length);
+
+    }
+
+    public static String stringLength(String word){
+        System.out.println(word.length());
+    }
+
+    public static String longestString(String num1, String num2)
+        if(num1.length > num2.length)
+
+    {
+        System.out.println(num1);
+    }
+
+    else if(num1.length < num2.length){
+        System.out.println(num2);
+    }
+
+
+public static String userInput(String command){
+
+}
+
+
+}
